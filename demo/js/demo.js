e78ee7d0bc8f9c12506908f35a4fa483a8f9dcda
ikait <ikai1026@gmail.com>
2013-06-08 12:51:26 +0900

    improved ui

--- demo/js/demo.js
+++ demo/js/demo.js
@@ -70,113 +70,137 @@ require(["jquery", "jquery.tabs", "../../lib/main"], function($) {
       });
     }());
 
-
     // 5章
     var chapter5 = (function () {
 
       // Optimization
       var optimization = new Optimization();
 
-      // 解の表現を作成
-      var expression = [];
+      // domain
+      var domain = (function () {
+
+        // domainの個数について、教科書には8と書いてあるのですが、
+        // schedule.txtには10個のスケジュールがあり(∴0～9)、正しくは9だと思われます
+        var domain = [], p = [0, 9];
+        for (var i = 0, j = optimization.people.length * 2; i < j; i += 1) {
+          domain.push(p);
+        }
+        return domain;
+      }());
+
+      // 結果を出力するコンストラクタ
+      var Demo = function (o) {
+        this.answer = o.answer;
+        this.$answer = o.$answer;
+        this.$table = o.$table;
+        this.$cost = o.$cost;
+        this.optimization = o.optimization || optimization;
+      };
+
+      Demo.prototype._getExpression = function () {
+        return '[' + this.answer.join(', ') + ']';
+      };
+
+      Demo.prototype._getTimeTable = function () {
+        var o = this.optimization.getschedule(this.answer);
+        var $table = $('<table>'), $tr = $('<tr>'), $td = $('<td>');
+        for (var i in o) {
+          var $_tr = $tr.clone()
+                      .append($td.clone().html(i))
+                      .append($td.clone().html(o[i]["origin"]))
+                      .append($td.clone().html(o[i]["out"]["time"]["dep"]))
+                      .append($td.clone().html("-"))
+                      .append($td.clone().html(o[i]["out"]["time"]["arr"]))
+                      .append($td.clone().html("$" + o[i]["out"]["price"]))
+                      .append($td.clone().html(o[i]["ret"]["time"]["dep"]))
+                      .append($td.clone().html("-"))
+                      .append($td.clone().html(o[i]["ret"]["time"]["arr"]))
+                      .append($td.clone().html("$" + o[i]["ret"]["price"]))
+                      .appendTo($table);
+        }
+        return $table.html();
+      };
+
+      Demo.prototype._getScheduleCost = function () {
+        return this.optimization.schedulecost(this.answer);
+      };
+
+      Demo.prototype.export = function () {
+        this.$answer.html(this._getExpression());
+        this.$table.html(this._getTimeTable());
+        this.$cost.html(this._getScheduleCost());
+      };
 
       // 5章のいずれかのボタンがclickされたときに計算
       $('#demo-form-chapter5').on('click', '[name*=submit]', function (e) {
         e.preventDefault();
-
-        // domain
-        var domain = (function () {
-
-          // domainの個数について、教科書には8と書いてあるのですが、
-          // schedule.txtには10個のスケジュールがあり(∴0～9)、正しくは9だと思われます
-          var domain = [], p = [0, 9];
-          for (var i = 0, j = optimization.people.length * 2; i < j; i += 1) {
-            domain.push(p);
-          }
-          return domain;
-        }());
-
-        // 解の表現[x, x, ..]を作って返す
-        var makeExpression = function (answer) {
-          return '[' + answer.join(', ') + ']';
-        };
-
-        // 表を作ってhtmlを返す関数
-        var makeTable = function (answer) {
-          var o = optimization.getschedule(answer);
-          var $table = $('<table>'), $tr = $('<tr>'), $td = $('<td>');
-          for (var i in o) {
-            var $_tr = $tr.clone()
-                        .append($td.clone().html(i))
-                        .append($td.clone().html(o[i]["origin"]))
-                        .append($td.clone().html(o[i]["out"]["time"]["dep"]))
-                        .append($td.clone().html("-"))
-                        .append($td.clone().html(o[i]["out"]["time"]["arr"]))
-                        .append($td.clone().html("$" + o[i]["out"]["price"]))
-                        .append($td.clone().html(o[i]["ret"]["time"]["dep"]))
-                        .append($td.clone().html("-"))
-                        .append($td.clone().html(o[i]["ret"]["time"]["arr"]))
-                        .append($td.clone().html("$" + o[i]["ret"]["price"]))
-                        .appendTo($table);
-          }
-          return $table.html();
-        };
 
         // 番号によって分岐させる
         switch ($(this).attr('name')) {
 
           // 5.2, 5.3
           case "c20_submit":
-            var c20_answer = [];
-
-            // inputのvalueから解をつくる
-            $('[name*="c20_input"]').each(function () {
-              c20_answer.push($(this).val());
+            var c20 = new Demo({
+              answer: (function () {
+                var r = [];
+                $('[name*="c20_input"]').each(function () {
+                  r.push($(this).val());
+                });
+                return r;
+              }()),
+              $answer: $('[name="c20_result_answer"]'),
+              $table: $('[name="c20_result"]'),
+              $cost: $('[name="c30_result"]')
             });
-
-            // 解を表示
-            $('[name="c20_result_answer"]').html(makeExpression(c20_answer));
-            $('[name="c20_result"]').html(makeTable(c20_answer));
-
-            // 5.3
-            var c30_result = optimization.schedulecost(c20_answer);
-            $('[name="c30_result"]').html(c30_result);
+            c20.export();
             break;
 
           // 5.4
           case "c40_submit":
-            var c40_answer = optimization.randomoptimize(domain, optimization.schedulecost);
-            $('[name="c40_result_answer"]').html(makeExpression(c40_answer));
-            $('[name="c40_result_table"]').html(makeTable(c40_answer));
-            $('[name="c40_result_cost"]').html(optimization.schedulecost(c40_answer));
+            var c40 = new Demo({
+              answer: optimization.randomoptimize(domain, optimization.schedulecost),
+              $answer: $('[name="c40_result_answer"]'),
+              $table: $('[name="c40_result_table"]'),
+              $cost: $('[name="c40_result_cost"]')
+            });
+            c40.export();
             break;
 
           // 5.5
           case "c50_submit":
-            var c50_answer = optimization.hillclimb(domain, optimization.schedulecost);
-            $('[name="c50_result_answer"]').html(makeExpression(c50_answer));
-            $('[name="c50_result_table"]').html(makeTable(c50_answer));
-            $('[name="c50_result_cost"]').html(optimization.schedulecost(c50_answer));
+            var c50 = new Demo({
+              answer: optimization.hillclimb(domain, optimization.schedulecost),
+              $answer: $('[name="c50_result_answer"]'),
+              $table: $('[name="c50_result_table"]'),
+              $cost: $('[name="c50_result_cost"]')
+            });
+            c50.export();
             break;
 
           // 5.6
           case "c60_submit":
-            var c60_answer = optimization.annealingoptimize(domain, optimization.schedulecost,
-                $('[name="c60_T"]').val(), $('[name="c60_cool"]').val(), $('[name="c60_step"]').val());
-            $('[name="c60_result_answer"]').html(makeExpression(c60_answer));
-            $('[name="c60_result_table"]').html(makeTable(c60_answer));
-            $('[name="c60_result_cost"]').html(optimization.schedulecost(c60_answer));
+            var c60 = new Demo({
+              answer: optimization.annealingoptimize(domain, optimization.schedulecost,
+                $('[name="c60_T"]').val(), $('[name="c60_cool"]').val(), $('[name="c60_step"]').val()),
+              $answer: $('[name="c60_result_answer"]'),
+              $table: $('[name="c60_result_table"]'),
+              $cost: $('[name="c60_result_cost"]')
+            });
+            c60.export();
             break;
 
           // 5.7
           case "c70_submit":
-            var c70_answer = optimization.geneticoptimize(domain, optimization.schedulecost,
+            var c70 = new Demo({
+              answer: optimization.geneticoptimize(domain, optimization.schedulecost,
                 $('[name="c70_popsize"]').val(), $('[name="c70_step"]').val(),
                 $('[name="c70_mutprob"]').val(), $('[name="c70_elite"]').val(),
-                $('[name="c70_maxiter"]').val());
-            $('[name="c70_result_answer"]').html(makeExpression(c70_answer));
-            $('[name="c70_result_table"]').html(makeTable(c70_answer));
-            $('[name="c70_result_cost"]').html(optimization.schedulecost(c70_answer));
+                $('[name="c70_maxiter"]').val()),
+              $answer: $('[name="c70_result_answer"]'),
+              $table: $('[name="c70_result_table"]'),
+              $cost: $('[name="c70_result_cost"]')
+            });
+            c70.export();
             break;
         }
 
